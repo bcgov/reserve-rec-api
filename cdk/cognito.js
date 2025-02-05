dbe830e0fd53f3f74565f389b822f9fcee0f2c13
Mark Lisé <mark@digitalspace.ca>
2025-02-05 12:30:20 -0800

    Add Cognito setup script
    - Created a new file `cdk/cognito.js`.
    - Added `cognitoSetup` function to configure a Cognito User Pool.
    - Configured user pool with email-only account recovery and OIDC provider.

---
+++ cdk/cognito.js
@@ -0,0 +1,31 @@
+const cdk = require('aws-cdk-lib');
+const cognito = require('aws-cdk-lib/aws-cognito');
+
+function cognitoSetup() {
+  console.log("Setting up Cognito...");
+
+  // Setup Cognito
+  const userPool = new cognito.UserPool(this, "UserPool", {
+    userPoolName: "AdminPool",
+    selfSignUpEnabled: false,
+    accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+    removalPolicy: cdk.RemovalPolicy.DESTROY
+  });
+
+  const oidcProvider = new cognito.UserPoolIdentityProviderOidc(this, "OidcProvider", {
+    userPool,
+    clientId: "backcountry",
+    issuerUrl: "https://dev.loginproxy.gov.bc.ca/auth/realms/bcparks-service-transformation",
+    attributeMapping: {
+      email: cognito.ProviderAttribute.other("email"),
+      givenName: cognito.ProviderAttribute.other("given_name"),
+      familyName: cognito.ProviderAttribute.other("family_name")
+    }
+  });
+
+  userPool.registerIdentityProvider(oidcProvider);
+}
+
+module.exports = {
+  cognitoSetup
+};
