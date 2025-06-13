05b897d6ff99667b3bc68f2c0cc50d39b59b6ead
Mark Lisé <marklise@users.noreply.github.com>
2025-06-13 13:54:42 -0700

    [65]  Refactor booking POST handler tests: move all jest.mock calls to the top before requires, update assertions to use statusCode and new response structure, and ensure proper mocking for createBooking and related dependencies. (#130)

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,30 @@
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/*.test.js'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^/opt/activities/configs$': '<rootDir>/lib/layers/dataUtils/activities/configs.js',
+    '^/opt/activities/methods$': '<rootDir>/lib/layers/dataUtils/activities/methods.js',
+    '^/opt/awsUtils/s3$': '<rootDir>/lib/layers/awsUtils/s3.js',
+    '^/opt/awsUtils/ses$': '<rootDir>/lib/layers/awsUtils/ses.js',
+    '^/opt/awsUtils/sns$': '<rootDir>/lib/layers/awsUtils/sns.js',
+    '^/opt/base$': '<rootDir>/lib/layers/base/base.js',
+    '^/opt/bookings/configs$': '<rootDir>/lib/layers/dataUtils/bookings/configs.js',
+    '^/opt/bookings/methods$': '<rootDir>/lib/layers/dataUtils/bookings/methods.js',
+    '^/opt/clients/configs$': '<rootDir>/lib/layers/dataUtils/clients/configs.js',
+    '^/opt/clients/methods$': '<rootDir>/lib/layers/dataUtils/clients/methods.js',
+    '^/opt/data-utils$': '<rootDir>/lib/layers/dataUtils/data-utils.js',
+    '^/opt/data-constants$': '<rootDir>/lib/layers/dataUtils/data-constants.js',
+    '^/opt/dynamodb$': '<rootDir>/lib/layers/awsUtils/dynamodb.js',
+    '^/opt/locations/configs$': '<rootDir>/lib/layers/dataUtils/locations/configs.js',
+    '^/opt/locations/methods$': '<rootDir>/lib/layers/dataUtils/locations/methods.js',
+    '^/opt/resources/configs$': '<rootDir>/lib/layers/dataUtils/resources/configs.js',
+    '^/opt/resources/methods$': '<rootDir>/lib/layers/dataUtils/resources/methods.js',
+    '^/opt/users/configs$': '<rootDir>/lib/layers/dataUtils/users/configs.js',
+    '^/opt/users/methods$': '<rootDir>/lib/layers/dataUtils/users/methods.js',
+    '^/opt/validation-rules$': '<rootDir>/lib/layers/dataUtils/validation-rules.js',
   }
 };
