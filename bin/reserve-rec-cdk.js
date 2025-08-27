989ff2082239f7ee676984a2446be4ea48b891b8
Mark Lisé <mark@digitalspace.ca>
2025-08-27 10:40:33 -0700

    Fix STACK_NAME

--- bin/reserve-rec-cdk.js
+++ bin/reserve-rec-cdk.js
@@ -3,7 +3,7 @@ const cdk = require('aws-cdk-lib');
 const { ReserveRecCdkStack } = require('../lib/reserve-rec-cdk-stack');
 
 const app = new cdk.App();
-new ReserveRecCdkStack(app, 'ReserveRecCdkStack', {
+new ReserveRecCdkStack(app, process.env.STACK_NAME, {
   env: {
     // AWS account variables
     account: process.env.CDK_DEFAULT_ACCOUNT,
