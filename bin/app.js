67ae0735eabd423d3a804a62d6b346a52c63e6a8
cameron <cameron@digitalspace.ca>
2025-10-11 00:33:11 +0000

    Refactoring for class based CDK algorithm

--- bin/app.js
+++ bin/app.js
@@ -1,90 +1,175 @@
-#!/usr/bin/env node
-
 const cdk = require('aws-cdk-lib');
-const { createCoreStack } = require('../lib/core-stack/core-stack');
-const { createAdminIdentityStack } = require('../lib/admin-identity-stack/admin-identity-stack');
-const { createPublicIdentityStack } = require('../lib/public-identity-stack/public-identity-stack');
-const { createAdminApiStack } = require('../lib/admin-api-stack/admin-api-stack');
-const { CONSTANTS } = require('../lib/constants.js');
+const { StackContext } = require('../lib/stack-context.js');
+const { createAdminApiStack } = require('../lib/admin-api-stack/admin-api-stack.js');
+const { createAdminIdentityStack } = require('../lib/admin-identity-stack/admin-identity-stack.js');
+const { logger } = require('../lib/utils');
+const { createCoreStack } = require('../lib/core-stack/core-stack.js');
+const { createPublicIdentityStack } = require('../lib/public-identity-stack/public-identity-stack.js');
 
-// Main function to build the CDK app
-async function buildApp() {
-  console.log('Starting CDK application synthesis...');
-
-  try {
-    // Create the CDK application
-    const app = new cdk.App();
-
-    // Initialize context objects
-    let context = {
-      CONSTANTS: CONSTANTS
+class ReserveRecApiApp {
+  constructor() {
+    this.app = new cdk.App();
+    this.appName = 'ReserveRecApi';
+    this.context = this.getContext();
+    this.stacks = {};
+    this.progress = {
+      completedStacks: [],
+      failedStacks: [],
+      totalStacks: 0,
+      currentStackKey: null,
     };
 
-    // Try to get context from cdk.json file
+    this.createStacks();
+  }
+
+  getAppName() {
+    return this.appName;
+  }
+
+  getDeploymentName() {
+    return this.context?.DEPLOYMENT_NAME || 'local';
+  }
+
+  getEnv() {
+
+  }
+
+  isOffline() {
+    return this.context?.IS_OFFLINE === 'true' || false;
+  }
+
+  getContext() {
     try {
-      let contextName = app.node.getContext('@context');
-      console.debug('Retrieving context for:', contextName);
-      context = { ...context, ...app.node.tryGetContext(contextName) };
+      let contextName = this.app.node.getContext('@context');
+      logger.debug('Retrieving context for:', contextName);
+      return this.app.node.tryGetContext(contextName);
     } catch (error) {
       throw error;
     }
+  }
 
+  getConfigValue(key, stackKey = null) {
+    // 'this' pertains to the current/bound stack
+    let self = this;
+    if (stackKey) {
+      self = this.getStackByKey(stackKey);
+    }
+    const config = self?.config;
+    if (config === null || config === undefined) {
+      logger.warn(`Config key ${key} not found in stack ${self?.stackKey}`);
+    }
+    return config[key];
+  }
 
+  getSecretValue(key, stackKey = null) {
+    // 'this' pertains to the current/bound stack
+    let self = this;
+    if (stackKey) {
+      self = this.getStackByKey(stackKey);
+    }
+    const secret = self?.secrets?.[key];
+    if (secret === null || secret === undefined) {
+      logger.warn(`Secret key ${key} not found in stack ${self?.stackKey}`);
+    }
+    return secret?.value || null;
+  }
 
-    const stacks = await createStacks(app, context);
+  getConstructByKey(constructKey, stackKey = null) {
+    // 'this' pertains to the current/bound stack
+    let self = this;
+    if (stackKey) {
+      self = this.getStackByKey(stackKey);
+    }
+    return self?.constructs?.[constructKey] || null;
+  }
 
-    configureDeploymentOrder(stacks);
+  getConstructId(constructKey, stackKey = null) {
+    // 'this' pertains to the current/bound stack
+    let self = this;
+    if (stackKey) {
+      self = this.getStackByKey(stackKey);
+    }
+    const construct = self.getConstructByKey(constructKey);
+    if (!construct) {
+      logger.warn(`Construct ${constructKey} not found in stack ${self.stackKey}`);
+    }
+    return construct?.id || null;
+  }
 
-  } catch (error) {
-    console.error('Error synthesizing stacks:', error);
+  getStackByKey(stackKey) {
+    return this.stacks[stackKey];
+  }
+
+  getCurrentStack() {
+    return this.stacks[this.progress.currentStackKey];
+  }
+
+  getStackId() {
+    return this.getCurrentStack()?.id;
+  }
+
+  async addStackContext(stackKey, props = {}) {
+    const stackContext = new StackContext(this, stackKey, props);
+    await stackContext.init();
+    this.stacks[stackKey] = stackContext;
+    return stackContext;
+  }
+
+  async createStacks() {
+    logger.info('Starting CDK application synthesis...\n');
+
+    await this.addStack('coreStack', createCoreStack);
+    await this.addStack('adminIdentityStack', createAdminIdentityStack);
+    await this.addStack('publicIdentityStack', createPublicIdentityStack);
+    // await this.addStack('adminIdentityStack', createAdminIdentityStack);
+    // await this.addStack('adminApiStack', createAdminApiStack);
+
+  }
+
+  async addStack(stackKey, stackCreateFn) {
+    try {
+      this.progress.totalStacks += 1;
+      this.progress.currentStackKey = stackKey;
+      logger.info(`Priming stack - Key: ${stackKey}`);
+      this.stacks[stackKey] = await stackCreateFn(this, stackKey);
+      this.progress.completedStacks.push(stackKey);
+      logger.info(`Stack created: ${stackKey}\n`);
+    } catch (error) {
+      this.progress.failedStacks.push(stackKey);
+      logger.error(`Error creating stack ${stackKey}:`, error);
+    }
+  }
+
+  createStackId(stackKey) {
+    let parts = [this.getAppName(), this.getDeploymentName(), stackKey];
+    for (const part of parts) {
+      if (!part) {
+        throw new Error(`Missing required part to construct stack ID: ${JSON.stringify(parts)}`);
+      }
+    }
+    return parts.map(part => part.charAt(0).toUpperCase() + part.slice(1)).join('-');
+  }
+
+  bindStackScope(stackKey, stackScope) {
+    this.stacks[stackKey] = stackScope;
+  }
+
+  getStackScope(stackKey) {
+    return this.stacks[stackKey];
+  }
+
+  initStackScope(stackKey, stackScope) {
+    this.bindStackScope(stackKey, stackScope);
+    return this.getStackScope(stackKey);
+  }
+
+  createScopedId(id, suffix = '-Construct') {
+    return id + suffix;
   }
 }
 
-async function createStacks(app, context) {
-  try {
+new ReserveRecApiApp();
 
-    let appConstants = context?.CONSTANTS;
-
-    // Core Stack
-    const coreStack = await createCoreStack(app, appConstants?.STACKS?.CORE_STACK?.STACK_NAME, context);
-
-    // Admin Identity Stack
-    const adminIdentityStack = await createAdminIdentityStack(app, appConstants?.STACKS?.ADMIN_IDENTITY_STACK?.STACK_NAME, context);
-
-    // Public Identity Stack
-    const publicIdentityStack = await createPublicIdentityStack(app, appConstants?.STACKS?.PUBLIC_IDENTITY_STACK?.STACK_NAME, context);
-
-    // Admin API Stack
-    const adminApiStack = await createAdminApiStack(app, appConstants?.STACKS?.ADMIN_API_STACK?.STACK_NAME, context);
-
-    return {
-      coreStack: coreStack,
-      adminIdentityStack: adminIdentityStack,
-      publicIdentityStack: publicIdentityStack,
-      adminApiStack: adminApiStack,
-    };
-
-  } catch (error) {
-    console.error('Error deploying stacks:', error);
-    return;
-  }
-}
-
-function configureDeploymentOrder(stacks) {
-  try {
-    // Order the stacks
-    stacks?.adminIdentityStack.addDependency(stacks?.coreStack);
-    stacks?.publicIdentityStack.addDependency(stacks?.coreStack);
-    stacks?.adminApiStack.addDependency(stacks?.coreStack);
-    stacks?.adminApiStack.addDependency(stacks?.adminIdentityStack);
-  } catch (error) {
-    console.error('Error establishing stack dependencies:', error);
-  }
-}
-
-buildApp();
-
-
-
-
-
+module.exports = {
+  ReserveRecApiApp
+};
--- src/layers/constructs.js
+++ src/layers/constructs.js
@@ -1,40 +1,85 @@
 const lambda = require('aws-cdk-lib/aws-lambda');
 const { RemovalPolicy } = require('aws-cdk-lib');
+const { Construct } = require('constructs');
 const path = require('path');
 const { buildDist } = require('../tools/bundling/yarnBundler');
 
 const LAMBDA_BASE_LAYER_PATH = './base';
+const LAMBDA_AWS_LAYER_PATH = './awsUtils';
 const NODEJS_PATH = 'nodejs';
 
-function createBaseLayer(scope, id, props) {
-  const constants = props?.CONSTANTS;
+class BaseLayerConstruct extends Construct {
+  constructor(scope, id, props) {
+    const constructId = scope.createScopedId(id);
 
-  const baseLayerEntry = path.join(__dirname, LAMBDA_BASE_LAYER_PATH);
-  const layer = new lambda.LayerVersion(scope, id, {
-    layerName: id,
-    code: lambda.Code.fromAsset(baseLayerEntry, {
-      bundling: {
-        image: lambda.Runtime.NODEJS_20_X.bundlingImage,
-        command: [],
-        local: {
-          tryBundle(outputDir) {
-            buildDist(baseLayerEntry, outputDir, {
-              env: props?.ENVIRONMENT_NAME,
-              nodejsPath: NODEJS_PATH,
+    super(scope, constructId, props);
+
+    this.baseLayerEntry = path.join(__dirname, LAMBDA_BASE_LAYER_PATH);
+    this.layer = new lambda.LayerVersion(scope, id, {
+      layerName: id,
+      code: lambda.Code.fromAsset(this.baseLayerEntry, {
+        bundling: {
+          image: lambda.Runtime.NODEJS_20_X.bundlingImage,
+          command: [],
+          local: {
+            tryBundle(outputDir) {
+              buildDist(this.baseLayerEntry, outputDir, {
+                env: props?.environment,
+                nodejsPath: NODEJS_PATH,
+              }
+              );
             }
-            );
           }
         }
-      }
-    }),
-    compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
-    description: `Base layer for Lambda functions in ${constants?.APP_NAME} - ${props?.ENVIRONMENT_NAME} environment`,
-    removalPolicy: RemovalPolicy.DESTROY,
-    license: 'Apache-2.0',
-  });
-  return layer;
+      }),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
+      // description: `Base layer for Lambda functions in ${props?.appName} - ${props?.deploymentName} environment`,
+      removalPolicy: RemovalPolicy.DESTROY,
+      license: 'Apache-2.0',
+    });
+  }
+
+  getLayer() {
+    return this.layer;
+  }
+}
+
+class AwsUtilsLayerConstruct extends Construct {
+  constructor(scope, id, props) {
+    const constructId = scope.createScopedId(id);
+    super(scope, constructId, props);
+
+    this.baseLayerEntry = path.join(__dirname, LAMBDA_BASE_LAYER_PATH);
+    this.layer = new lambda.LayerVersion(scope, id, {
+      layerName: id,
+      code: lambda.Code.fromAsset(this.baseLayerEntry, {
+        bundling: {
+          image: lambda.Runtime.NODEJS_20_X.bundlingImage,
+          command: [],
+          local: {
+            tryBundle(outputDir) {
+              buildDist(this.baseLayerEntry, outputDir, {
+                env: props?.environment,
+                nodejsPath: NODEJS_PATH,
+              }
+              );
+            }
+          }
+        }
+      }),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
+      description: `Base layer for Lambda functions in ${props?.appName} - ${props?.deploymentName} environment`,
+      removalPolicy: RemovalPolicy.DESTROY,
+      license: 'Apache-2.0',
+    });
+
+  }
+  getLayer() {
+    return this.layer;
+  }
 }
 
 module.exports = {
-  createBaseLayer,
+  BaseLayerConstruct,
+  AwsUtilsLayerConstruct,
 };
