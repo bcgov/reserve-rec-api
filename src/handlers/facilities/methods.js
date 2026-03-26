7aa42dcf04671ea53c8e3455e98da7e498e4f38b
Dave <62899351+davidclaveau@users.noreply.github.com>
2026-03-26 15:36:44 -0700

    fix error with fetching facilities without activity relations (#338)
    Signed-off-by: davidclaveau <daveclaveau@gmail.com>

--- src/handlers/facilities/methods.js
+++ src/handlers/facilities/methods.js
@@ -174,8 +174,13 @@ async function getFacilityByFacilityId(collectionId, facilityType, facilityId, f
         },
       });
 
-      const activityKeys = activityRelationships.items.map(rel => ({ pk: rel.pk2, sk: rel.sk2 }));
-      res.activities = await batchGetData(activityKeys, REFERENCE_DATA_TABLE_NAME);
+      // If there are activity relationships, batch get the activities and add them to the response
+      if (activityRelationships?.Items?.length > 0) {
+        const activityKeys = activityRelationships.items.map(rel => ({ pk: rel.pk2, sk: rel.sk2 }));
+        res.activities = await batchGetData(activityKeys, REFERENCE_DATA_TABLE_NAME);
+      } else {
+        res.activities = [];
+      }
     }
     
     logger.debug(`Facility: ${JSON.stringify(res, null, 2)}`);
