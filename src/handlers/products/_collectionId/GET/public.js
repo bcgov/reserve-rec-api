a4ee55912afed7d99302ac2965ea94528237d5d3
davidclaveau <daveclaveau@gmail.com>
2026-04-01 22:33:37 +0000

    add missing public file

---
+++ src/handlers/products/_collectionId/GET/public.js
@@ -0,0 +1,34 @@
+const { fetchProducts } = require("../../methods");
+const { logger, sendResponse, checkAuthContext } = require("/opt/base");
+
+/**
+ * @api {get} /products/{collectionId} GET
+ * Fetch Products Public
+ */
+exports.handler = async (event, context) => {
+  logger.info(`GET Products Public: ${event}`);
+
+  if (event?.httpMethod === "OPTIONS") {
+    return sendResponse(200, null, "Success", null, context);
+  }
+
+  const collectionId = event?.pathParameters?.collectionId;
+  const activityType = event?.pathParameters?.activityType || event?.queryStringParameters?.activityType || null;
+  const activityId = event?.pathParameters?.activityId || event?.queryStringParameters?.activityId || null;
+  const productId = event?.pathParameters?.productId || event?.queryStringParameters?.productId || null;
+  const { ...queryParams } = event?.queryStringParameters || {};
+
+  try {
+    const res = await fetchProducts(collectionId, activityType, activityId, productId, queryParams)
+
+    return sendResponse(200, res, "Success", null, context);
+  } catch (error) {
+    return sendResponse(
+      Number(error?.code) || 400,
+      error?.data || null,
+      error?.message || "Error",
+      error?.error || error,
+      context
+    );
+  }
+};
